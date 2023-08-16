650125a21c4765aee6e16662a2d6c5c0c8d70cf4
MelvinCou <113338357+MelvinCou@users.noreply.github.com>
2023-08-16 10:49:37 +0200

    chezmoi

--- .zshrc
+++
@@ -1,6 +0,0 @@
-antigen bundle zsh-users/zsh-completions
-antigen theme romkatv/powerlevel10k
-antigen apply
-
-autoload -U compinit
-compinit
--- install.sh
+++ install.sh
@@ -1,17 +1,30 @@
-#!/bin/bash
+#!/bin/sh
 
-# Get font for powerlevel10k
-set  -euo pipefail
-I1FS=$'\n\t'
-echo "Installing MesloGS font"
-fontpath="${XDG_DATA_HOME:-$HOME/.local/share}"/fonts/MesloGS
-mkdir -p $fontpath && cd $fontpath
-wget -q https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
-wget -q https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
-wget -q https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
-wget -q https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
-fc-cache -f
+# -e: exit on error
+# -u: exit on unset variables
+set -eu
 
-echo "Installing packages"
-sudo apt update -qq
-sudo apt install -qqq --no-install-recommends zsh zsh-antigen bat
+if ! chezmoi="$(command -v chezmoi)"; then
+	bin_dir="${HOME}/.local/bin"
+	chezmoi="${bin_dir}/chezmoi"
+	echo "Installing chezmoi to '${chezmoi}'" >&2
+	if command -v curl >/dev/null; then
+		chezmoi_install_script="$(curl -fsSL get.chezmoi.io)"
+	elif command -v wget >/dev/null; then
+		chezmoi_install_script="$(wget -qO- get.chezmoi.io)"
+	else
+		echo "To install chezmoi, you must have curl or wget installed." >&2
+		exit 1
+	fi
+	sh -c "${chezmoi_install_script}" -- -b "${bin_dir}"
+	unset chezmoi_install_script bin_dir
+fi
+
+# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
+script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+
+set -- init --apply --source="${script_dir}"
+
+echo "Running 'chezmoi $*'" >&2
+# exec: replace current process with chezmoi
+exec "$chezmoi" "$@"
