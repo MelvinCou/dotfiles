d735639a9cd9ac95e5c4becf8c21ba618128a3e6
MelvinCou <113338357+MelvinCou@users.noreply.github.com>
2026-07-17 14:23:23 +0200

    Remove manual winget completion

--- home/private_PowerShell/Microsoft.PowerShell_profile.ps1
+++ home/private_PowerShell/Microsoft.PowerShell_profile.ps1
@@ -141,14 +141,3 @@ $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
     Import-Module "$ChocolateyProfile"
 }
-
-# Winget tab-completion
-Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
-    param($wordToComplete, $commandAst, $cursorPosition)
-        [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
-        $Local:word = $wordToComplete.Replace('"', '""')
-        $Local:ast = $commandAst.ToString().Replace('"', '""')
-        winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
-            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-        }
-}
